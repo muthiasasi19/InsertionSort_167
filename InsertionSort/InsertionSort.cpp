330a1f6b68220fa2c52c9198f9a7f70c943d7c20
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-03-24 11:37:08 -0700

    prosedur untuk meng-input

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -3,3 +3,18 @@ using namespace std;
 
 int arr[20];							// Membuat Array dengan panjang data 20
 int n;								   //  Membuat variable inputan n
+
+void input() {						//	Procedure Input 
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : "; // Membuat inputan jumlah element Array 
+		cin >> n;										// Memanggil variable inputan n 
+
+		if (n <= 20) {									// Membuat kondisi n tidak boleh lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 Element.\n"; // Menampilkan Pesan jika data lebih dari 20 
+		}
+	}
