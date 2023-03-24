1b064e80a19b0ea34ddf3edfe4626be44c52d540
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-03-24 11:54:51 -0700

    Menampilkan pesan pada layar output

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -18,3 +18,16 @@ void input() {						//	Procedure Input
 			cout << "\nArray yang anda masukkan maksimal 20 Element.\n"; // Menampilkan Pesan jika data lebih dari 20 
 		}
 	}
+	cout << endl;										// Membuat jarak per baris program
+	cout << "======================" << endl;			// Membuat tampilan susunan data element array 
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+
+	for (int i = 0; i < n; i++)						// Menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";		// Menyimpan nilai data n pada layar 
+		cin >> arr[i];								// Menyimpan nilai data n kedalan array arr
+	}
+}
+
