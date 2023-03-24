87d2e5bb59b24c4496e2b3854b48fccd3dd4602f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-03-24 12:42:21 -0700

    kurang 1 baris

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -48,7 +48,11 @@ void insertionsort(){					// Procedure Insertionsort
 			arr[j + 1] = arr[j];		// 4a. Simpan arr[j] ke dalam variable arr[j+1]
 			j--;						// 4b. Decrement nilai j by 1
 		}
+
 		arr[j + 1] = temp;			// 5. Simpan nilai temp ke dalam arr[j+1]
+
+
+
 
 	}
 
